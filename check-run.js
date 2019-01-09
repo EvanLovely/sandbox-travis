0932b4bd745ef689e7c656ba93b7089b50a9dad3
Evan Lovely <evanlovely@gmail.com>
2019-01-08 21:07:16 -0800

    node check runs

---
+++ check-run.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { getGitSha } = require('ci-utils');
+
+const installationId = '23168';
+const repoSlug = `evanlovely/sandbox-travis`;
+const privateKey = fs.readFileSync(
+  './test-basalt-github-app.2019-01-08.private-key.pem',
+  'utf8',
+);
+
+const jwtToken = jwt.sign(
+  {
+    iss: installationId,
+    // exp: Math.floor(Date.now() / 1000) + (10 * 60),
+  },
+  privateKey,
+  {
+    algorithm: 'RS256',
+    expiresIn: '10m',
+  },
+);
+
+const getHeaders = token => ({
+  'Content-Type': 'application/json',
+  Accept:
+    'application/vnd.github.antiope-preview+json, application/vnd.github.machine-man-preview+json, application/vnd.github.v3+json',
+  Authorization: `Bearer ${token}`,
+});
+
+/**
+ * @param {string} installationId
+ * @return {Promise<{token: string, expires_at: string}>}
+ */
+async function getAccessToken(installationId = '') {
+  try {
+    return fetch(
+      `https://api.github.com/app/installations/${installationId}/access_tokens`,
+      {
+        method: 'POST',
+        headers: {
+          ...getHeaders(jwtToken),
+        },
+      },
+    ).then(res => res.json());
+  } catch (err) {
+    console.error('error getting access token ', err);
+    process.exit(1);
+  }
+}
+
+async function setCheckRun({ name, status, token }) {
+  try {
+    return fetch(`https://api.github.com/repos/${repoSlug}/check-runs`, {
+      method: 'POST',
+      headers: {
+        ...getHeaders(token),
+      },
+      body: JSON.stringify({
+        name,
+        status,
+        head_sha: getGitSha(),
+      }),
+    }).then(res => res.json());
+  } catch (err) {
+    console.error('error setting check run ' + name + ' to ' + status, err);
+    process.exit(1);
+  }
+}
+
+async function go() {
+  const { token } = await getAccessToken('561764');
+  if (!token) {
+    console.error('uh oh, no token!');
+    process.exit(1);
+  }
+  const results = await setCheckRun({
+    name: 'ima tester',
+    status: 'in_progress',
+    token,
+  });
+  console.log('setCheckRun results', results);
+}
+
+go();
